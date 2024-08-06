ec647dd570e4c73ef5d50f4b202dfd723f186414
DevWebAbhi <tiwariabhishek889912@gmail.com>
2024-08-07 00:24:13 +0530

    Abhishek

--- src/app/api/getVlogs/route.js
+++ src/app/api/getVlogs/route.js
@@ -1,40 +1,27 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { executeQuery } from "@/dbconfig/db";
-import { cookies } from 'next/headers';
 export const dynamic = 'force-dynamic';
 
 export async function GET(req) {
     try {
-        const cookieStore = cookies();
-        const token = cookieStore.get('nextvlogauthtoken'); 
-        
-        console.log("Token from cookies:", token);
+
 
         let allvlogs;
         const { searchParams } = new URL(req.url);
         const page = searchParams.get('page');
-        if (token && token.value) {
-                let decoded;
-                try {
-                     decoded = jwt.verify(token.value, process.env.JWT_CODE);
-                    console.log("Decoded Token:", decoded);
-                } catch (error) {
-                    return NextResponse.json({ message: "TEXP" }, { status: 401 });
-                }
+       
 
-                const { email } = decoded;
-                allvlogs = await executeQuery({
-                    query: `CALL nextvlog.AllVlogs(?,?,?)`,
-                    values: [email, token.value, page || 1],
-                });
+               
             
-        } else {
+      
             allvlogs = await executeQuery({
                 query: `CALL nextvlog.AllVlogs(?,?,?)`,
                 values: [null, null, page || 1],
             });
-        }
+            if(allvlogs && allvlogs[0] && allvlogs[0].length==0){
+                return NextResponse.json({ message: 'NOV' }, { status: 404 });
+              }
 
         allvlogs.splice(-1);
 
