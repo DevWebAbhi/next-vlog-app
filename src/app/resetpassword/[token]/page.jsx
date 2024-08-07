729f346dd3d13923f20d8c0f08776a9c53ececcf
DevWebAbhi <tiwariabhishek889912@gmail.com>
2024-08-07 13:58:47 +0530

    Abhishek

--- src/app/resetpassword/[token]/page.jsx
+++ src/app/resetpassword/[token]/page.jsx
@@ -10,7 +10,7 @@ import { useParams } from "next/navigation";
 import { passwordResetSchema } from "@/zodValidations/zodValidations";
 import { toastPopUp } from "@/app/CommonFunctions/popupInfo";
 import Spinner from "@/app/Spinner";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 const Page = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
